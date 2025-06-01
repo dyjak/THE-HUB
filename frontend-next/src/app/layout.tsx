d0b56ec6e1d4cf437ee266c0e7efbbd2072b91af
dyjak <dyyyak@gmail.com>
2025-06-01 20:19:55 +0200

    updates

--- frontend-next/src/app/layout.tsx
+++ frontend-next/src/app/layout.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import "./globals.css";
 import { Orbitron } from "next/font/google";
-import { Pompiere } from "next/font/google";
-import { Turret_Road } from "next/font/google";
 import { Providers } from "./providers";
 import Layout from "@/components/Layout";
 
 const orbitron = Orbitron({ subsets: ["latin"], weight: ["400", "700"] });
-const pompiere = Pompiere({ subsets: ["latin"], weight: ["400"] });
-const turret_road = Turret_Road({ subsets: ["latin"], weight: ["800"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="pl">
-        <body className={`${turret_road.className} bg-black text-white`}>
+        <body className={`${orbitron.className} bg-black text-white`}>
             <Providers>
                 <Layout>{children}</Layout>
             </Providers>
